fc7d1e5daca45fceffdbf72f3b69d0071ad013cd
Noid3ah <91058949+Noid3ah@users.noreply.github.com>
2022-11-30 22:17:22 -0500

    .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Practice-Projects
+#To-Do list app.
+
+- This is the first project i built on my own.
---
+++ app.js
@@ -0,0 +1,9 @@
+const userInput_Elem = document.getElementById('input');
+const addListButton__Elem = document.querySelector('.add-btn');
+const listOfToDo = document.querySelector('.list');
+
+function addButtonHandler() {}
+
+/*=================== Event Listeners ===================*/
+
+addListButton__Elem.addEventListener('click', addButtonHandler);
