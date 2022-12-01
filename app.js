238f8c1cd381bedd8f4a96ad4a7d77300d740c5d
Noid3ah <91058949+Noid3ah@users.noreply.github.com>
2022-12-01 17:18:09 -0500

    Added functionality to delete a task.

--- app.js
+++ app.js
@@ -1,9 +1,55 @@
-const userInput_Elem = document.getElementById('input');
-const addListButton__Elem = document.querySelector('.add-btn');
-const listOfToDo = document.querySelector('.list');
+const button = document.getElementById('button');
 
-function addButtonHandler() {}
+function getInput() {
+  const input = document.getElementById('input');
 
-/*=================== Event Listeners ===================*/
+  const userInput = input.value; //?
+  renderListElement(userInput); //?
+}
 
-addListButton__Elem.addEventListener('click', addButtonHandler);
+function renderListElement(el) {
+  el.trim();
+  const li = document.createElement('li');
+  li.textContent = el;
+
+  if (!el) {
+    return;
+  }
+
+  // const all_Li = document.querySelectorAll(li);
+
+  updateUI(li);
+  renderDeleteBtn(li);
+  // deleteTask(li);
+}
+
+function updateUI(el) {
+  const ol = document.querySelector('.list');
+
+  ol.append(el);
+  clearInput();
+}
+
+function clearInput() {
+  input.value = '';
+}
+
+function renderDeleteBtn(element) {
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerText = 'delete';
+  deleteBtn.className = 'not-visible';
+
+  element.addEventListener('click', () => {
+    element.append(deleteBtn);
+    deleteBtn.classList.toggle('visible');
+    deleteTask(element, deleteBtn);
+  });
+}
+
+function deleteTask(listItem, btn) {
+  btn.addEventListener('click', () => {
+    listItem.parentElement.removeChild(listItem);
+  });
+}
+
+button.addEventListener('click', getInput);
